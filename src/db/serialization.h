d26d73d0c714bc04a40624f0cb5c1d6f71f61fdf
Florin Iucha <florin@signbit.net>
2019-08-01 22:22:55 -0400

    Document the serialization format

---
+++ src/db/serialization.h
@@ -0,0 +1,37 @@
+/*
+   Copyright 2019 Florin Iucha
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef SERIALIZATION_H_INCLUDED
+#define SERIALIZATION_H_INCLUDED
+
+#include <cstdint>
+
+struct SerializedObjectHeader
+{
+   // 128 bit hash of the rest of the header + body; uses SpookyV2 Hash
+   uint64_t m_hash[2];
+
+   // object name or uuid or whatever
+   char m_objectType[16];
+
+   // the version of serialization for this type
+   uint64_t m_version;
+
+   // 64 bit object size
+   uint64_t m_size;
+};
+
+#endif // SERIALIZATION_H_INCLUDED
