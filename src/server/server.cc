4cd9a318203ecca1dee6c98c7d3d62abd5a744cf
Florin Iucha <florin@signbit.net>
2019-08-11 22:19:29 -0400

    Add missing license headers

--- src/server/server.cc
+++ src/server/server.cc
@@ -1,3 +1,19 @@
+/*
+   Copyright 2019 Florin Iucha
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #include <ftags.pb.h>
 
 #include <project.h>
@@ -52,7 +68,7 @@ int main(int argc, char* argv[])
       //  Wait for next request from client
       socket.recv(&request);
       command.ParseFromArray(request.data(), static_cast<int>(request.size()));
-      std::cout << "Received request from " << command.source() << std::endl;
+      spdlog::info("Received request from {}: {}", command.source(), command.Type_Name(command.type()));
 
       if (command.type() == ftags::Command_Type::Command_Type_QUERY)
       {
