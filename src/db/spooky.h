080996303c03070b16b61d9b9e8d75fbcd765878
Florin Iucha <florin@signbit.net>
2019-08-26 21:15:01 -0400

    Wrap SpookyV2.h into a file with header guards

---
+++ src/db/spooky.h
@@ -0,0 +1,22 @@
+/*
+   Copyright 2019 Florin Iucha
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef FTAGS_DB_SPOOKY_H_INCLUDED
+#define FTAGS_DB_SPOOKY_H_INCLUDED
+
+#include <SpookyV2.h>
+
+#endif // FTAGS_DB_SPOOKY_H_INCLUDED
